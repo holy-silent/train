97a8eea602325be7c0ddbc85d2fe49d3ddcddcb2
gzq516 <972901428@qq.com>
2017-07-31 14:38:02 +0800

    高中谦

--- SManger/src/main/java/com/unicom/controller/ScoreController.java
+++ SManger/src/main/java/com/unicom/controller/ScoreController.java
@@ -48,7 +48,7 @@ public class ScoreController {
 
 		List<StuScore> stus = scoreService.getStuList(couId,stuName,currPage,page_size);
 
-		//获得总行数
+		//获得总行数123
 		String count = scoreService.getTotalCount(couId,stuName);
 
 		request.setAttribute("students", stus);
