890b81bdbfc03890e54a5d655b7b992cc95cb038
lic92 <lic92@chinaunicom.cn>
2017-07-31 15:03:42 +0800

    呸呸呸

--- SManger/src/main/java/com/unicom/controller/StudentController.java
+++ SManger/src/main/java/com/unicom/controller/StudentController.java
@@ -44,7 +44,7 @@ public class StudentController {
 		int page_size = Integer.parseInt(servletContext.getInitParameter("pagesize"));
 		//获得列表
 		//学生列表
-		//1
+		//1000000000000000
 		int currPage = Integer.parseInt(request.getParameter("currPage"));
 		String stuName = request.getParameter("stuName");
 		if(stuName!=null)
