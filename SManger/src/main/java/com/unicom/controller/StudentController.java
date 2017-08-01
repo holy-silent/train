b99584977b36a2b076c698e05e7e3e285fdf4365
Tsingmo <lenglangxue@163.com>
2017-08-01 10:33:30 +0800

    Gaowang

--- SManger/src/main/java/com/unicom/controller/StudentController.java
+++ SManger/src/main/java/com/unicom/controller/StudentController.java
@@ -28,7 +28,6 @@ import com.unicom.service.StudentService;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import static javafx.scene.input.KeyCode.T;
-////////dddddddddddd
 @Controller
 @RequestMapping("/student")
 public class StudentController {
